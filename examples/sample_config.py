30774b0117ee0b94eb3fa57b0fa12dd1d1d02d46
vspaz <vladimir.edelman@gmail.com>
2021-10-11 22:05:27 +0200

    add sample config

---
+++ examples/sample_config.py
@@ -0,0 +1,18 @@
+config = {
+    "http": {
+        "timeouts": {
+            "read": 5,
+            "connect": 5,
+        },
+    },
+    "basic_auth": {
+        "user": "user",
+        "password": "password",
+    },
+    "tls": {
+        "ca_path": "path/to/ca",
+        "client_certificate_path": "path/to/client/certificate",
+        "client_key_path": "path/to/key",
+    },
+    "user_agent": "",
+}
