223d5776d12a9c3c88d86e7c782093c56e330288
vspaz <vladimir.edelman@gmail.com>
2021-10-11 21:50:14 +0200

    add Pyclient class

--- pclient/http.py
+++ pclient/http.py
@@ -1,4 +1,27 @@
-class PyClient:
-    def __init__(self):
-        pass
+import requests
+import ujson
 
+from urllib3.util.retry import Retry
+
+requests.models.complexjson = ujson
+
+
+class PyClient:
+    def __init__(self, config: dict):
+        self._session: requests.Session = requests.Session()
+        self._session.mount("https://", self._retry_on(config=config))
+        self._session.mount("http://", self._retry_on(config=config))
+
+    @staticmethod
+    def _retry_on(config: dict):
+        return requests.adapters.HTTPAdapter(
+            max_retires=Retry(
+                total=config.get("retries", 3),
+                backoff_factor=config.get("backoff", 0.5),
+                status_forcelist=config.get("on_errors", []),
+            )
+        )
+
+    @staticmethod
+    def get_http_client(config: dict):
+        return PyClient(config=config)
