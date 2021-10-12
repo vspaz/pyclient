12a9236f60b9cbadb319fe508472af5f7c1f22c8
vspaz <vladimir.edelman@gmail.com>
2021-10-12 22:12:34 +0200

    add typing

--- pyclient/http.py
+++ pyclient/http.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Union, Dict, Tuple
+
 import requests
 import ujson
 from urllib3.util.retry import Retry
@@ -6,25 +10,28 @@ requests.models.complexjson = ujson
 
 
 class PyClient:
-    def __init__(self, config: dict):
+    def __init__(self, config: Dict[str, dict]) -> None:
         self._validate_config(config=config)
         self._session: requests.Session = requests.Session()
-        http_config = config['http']
-        port = str(http_config.get('port', ''))
+        http_config: dict = config['http']
+        port: Union[str, int] = str(http_config.get('port', ''))
 
         self._session.mount(
             prefix=f"{http_config['host']}" + f":{port}/" if port else '/',
             adapter=self._retry_on(config=http_config.get('retries', {})),
         )
-        tls_certificates = config.get('tls', {})
+        tls_certificates: dict = config.get('tls', {})
         if tls_certificates:
             self._session.verify = tls_certificates['ca_path']
             self._session.cert = (
                 tls_certificates['client_certificate_path'],
                 tls_certificates['client_key_path'],
             )
-        timeouts = http_config.get('timeouts', {})
-        self._timeouts = (timeouts.get('connect', 5), timeouts.get('read', 10))
+        timeouts: Dict[str, int] = http_config.get('timeouts', {})
+        self._timeouts: tuple = (
+            timeouts.get('connect', 5),
+            timeouts.get('read', 10),
+        )
 
     @staticmethod
     def _retry_on(config: dict) -> requests.adapters.HTTPAdapter:
@@ -36,7 +43,7 @@ class PyClient:
             ),
         )
 
-    def _request(self, url, method, **kwargs):
+    def _request(self, url: str, method: str, **kwargs: dict) -> requests.Response:
         return self._session.request(
             method=method,
             url=url,
@@ -45,10 +52,10 @@ class PyClient:
         )
 
     @staticmethod
-    def _validate_config(config: dict):
+    def _validate_config(config: Dict[str, dict]) -> dict:
         # TODO: add config validation.
         pass
 
     @staticmethod
-    def get_http_client(config: dict):
+    def get_http_client(config: Dict[str, dict]) -> PyClient:
         return PyClient(config=config)
