86c1d7ea06fc7f4ce22dd8dd8b225971d7a0dafc
vspaz <vladimir.edelman@gmail.com>
2021-10-16 16:13:27 +0200

    add 'validate_config' decorator

--- pyclient/config.py
+++ pyclient/config.py
@@ -1,5 +1,19 @@
+import functools
+
 from jsonschema import Draft4Validator, validate
 
 
 def _validate_config(config, schema):
     validate(instance=config, schema=schema, cls=Draft4Validator)
+
+
+def validate_config(schema):
+    def _wrapper(func):
+        @functools.wraps(func)
+        def __wrapper(*args, **kwargs):
+            _validate_config(config=kwargs['config'], schema=schema)
+            return func(*args, **kwargs)
+
+        return __wrapper
+
+    return _wrapper
