1d41a7aed5ffa57ad81d0e4e0cc41fa8cab7e2ee
vspaz <vladimir.edelman@gmail.com>
2021-10-16 16:26:31 +0200

    add config validation schema

--- pyclient/config.py
+++ pyclient/config.py
@@ -17,3 +17,69 @@ def validate_config(schema):
         return __wrapper
 
     return _wrapper
+
+
+_TIMEOUTS = {
+    'type': 'object',
+    'properties': {
+        'connect': {
+            'type': 'number',
+        },
+        'read': {
+            'type', 'number',
+        },
+    }
+}
+
+_HTTP = {
+    'type': 'object',
+    'properties': {
+        'host': {
+            'type': 'string',
+        },
+        'port': {
+            'type': ['number', 'string', ]
+        },
+        'timeouts': _TIMEOUTS,
+    },
+    'required': ['host', ],
+    'additionalProperties': False
+}
+
+_BASIC_AUTH = {
+    'type': 'object',
+    'properties': {
+        'user': {
+            'type': 'string',
+        },
+        'password': {
+            'type': 'string',
+        },
+    }
+}
+
+_TLS = {
+    'type': 'object',
+    'properties': {
+        'ca_path': {
+            'type': 'string',
+        },
+        'client_certificate_path': {
+            'type': 'string',
+        },
+        'client_key_path': {
+            'type': 'string',
+        },
+    }
+}
+
+CONFIG_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'http': _HTTP,
+        'basic_auth': _BASIC_AUTH,
+        'tls': _TLS,
+    },
+    'required': ['http', ],
+    'additionalProperties': False,
+}
