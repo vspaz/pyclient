070d76d02935287f4c41c1bfe306462f76440dd6
vspaz <vladimir.edelman@gmail.com>
2021-10-17 19:39:13 +0200

    add a test case for an empty config

--- tests/test_config.py
+++ tests/test_config.py
@@ -23,3 +23,8 @@ def test_host_missing_fail():
 
     with pytest.raises(jsonschema.ValidationError):
         schemas._validate_config(config=config, schema=schemas.CONFIG_SCHEMA)
+
+
+def test_config_empty_fail():
+    with pytest.raises(jsonschema.ValidationError):
+        schemas._validate_config(config=dict(), schema=schemas.CONFIG_SCHEMA)
