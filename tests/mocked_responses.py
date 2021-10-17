2d50dc5ccafa40b87cc003ad7437b4eb9ee2b15e
vspaz <vladimir.edelman@gmail.com>
2021-10-17 22:04:11 +0200

    add a test case for 'http_client.do_get'

--- tests/mocked_responses.py
+++ tests/mocked_responses.py
@@ -12,7 +12,7 @@ def _response_factory(path: str):
             'json.return_value': {"status": "accepted"},
             'text': str({"status": "accepted"}),
             'status_code': 202,
-        }
+        },
     }
     mocked_response = path_to_response.get(path)
     assert mocked_response, 'response not yet defined'
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,6 @@
 from unittest import mock
 
 from pyclient.http import PyClient
-
 from .mocked_responses import mocked_http_calls
 
 
@@ -28,3 +27,18 @@ def test_do_get_ok(mock_do_get):
     assert resp.text == "{'foo': 'bar'}"
     assert resp.status_code == 200
     assert resp.json() == {"foo": "bar"}
+
+
+@mock.patch('pyclient.http.PyClient.do_post', side_effect=mocked_http_calls)
+def test_do_post_ok(mock_do_get):
+    http_client = PyClient.get_http_client(
+        config={
+            'http': {
+                'host': 'http://example.com',
+            },
+        },
+    )
+    resp = http_client.do_post("/post", json={"foo", "bar"})
+    assert resp.text == "{'status': 'accepted'}"
+    assert resp.status_code == 202
+    assert resp.json() == {"status": "accepted"}
