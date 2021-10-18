fe609b70bf944381ec81d489a8af1078e5e6fae8
vspaz <vladimir.edelman@gmail.com>
2021-10-18 20:12:07 +0200

    remove duplicated code

--- tests/mocked_responses.py
+++ tests/mocked_responses.py
@@ -2,32 +2,21 @@ from unittest import mock
 
 
 def _response_factory(path: str):
+    common_mock_response = {
+        'json.return_value': {'status': 'accepted'},
+        'text': str({'status': 'accepted'}),
+        'status_code': 202,
+    }
     path_to_response = {
         '/get': {
             'json.return_value': {'foo': 'bar'},
             'text': str({'foo': 'bar'}),
             'status_code': 200,
         },
-        '/post': {
-            'json.return_value': {'status': 'accepted'},
-            'text': str({'status': 'accepted'}),
-            'status_code': 202,
-        },
-        '/patch': {
-            'json.return_value': {'status': 'accepted'},
-            'text': str({'status': 'accepted'}),
-            'status_code': 202,
-        },
-        '/put': {
-            'json.return_value': {'status': 'accepted'},
-            'text': str({'status': 'accepted'}),
-            'status_code': 202,
-        },
-        '/delete': {
-            'json.return_value': {'status': 'accepted'},
-            'text': str({'status': 'accepted'}),
-            'status_code': 202,
-        },
+        '/post': common_mock_response,
+        '/patch': common_mock_response,
+        '/put': common_mock_response,
+        '/delete': common_mock_response,
     }
     mocked_response = path_to_response.get(path)
     assert mocked_response, 'response not yet defined'
