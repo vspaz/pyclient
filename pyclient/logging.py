8b19187539f269320f2b5a36fca94fb7bf5b2d35
vspaz <vladimir.edelman@gmail.com>
2021-10-21 21:05:58 +0200

    add 'log_request' decorator

--- pyclient/logging.py
+++ pyclient/logging.py
@@ -1,5 +1,9 @@
+import functools
 import logging
 import sys
+
+import requests
+import ujson
 
 logging.basicConfig(
     stream=sys.stderr,
@@ -8,4 +12,22 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %X',
 )
 
-_LOGGER = logging.getLogger(__name__)
+
+def log_request(func):
+    logger = logging.getLogger(__name__)
+
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        logger.debug(f'request url: {kwargs.get("url")!r}')
+        resp: requests.Response = func(*args, **kwargs)
+        logger.debug(f'request headers: {ujson.dumps(dict(resp.request.headers))}')
+
+        if body := resp.request.body:
+            logger.debug(f'request body: {body.decode()}')
+
+        logger.debug(f'status code: {resp.status_code}')
+        logger.debug(f'response headers: {resp.headers}')
+        logger.debug(f'response body: {resp.text}')
+        return resp
+
+    return _wrapper
