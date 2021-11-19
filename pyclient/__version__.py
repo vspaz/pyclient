6f9b6427e17ff861747b1f480610777655a394c0
vspaz <vladimir.edelman@gmail.com>
2021-11-19 03:49:16 +0100

    fix description

--- pyclient/__version__.py
+++ pyclient/__version__.py
@@ -1,10 +1,10 @@
 __title__ = 'pyclient'
 __description__ = (
-    'pyclient, an http client that supports '
+    'pyclient, an HTTP client that supports '
     'easy configuration of tls, basic auth, retries, etc'
 )
 __url__ = 'https://github.com/vspaz/pyclient'
-__email__ = ''
+__email__ = 'wlodzimierz.trzmielinsky@gmail.com'
 __author__ = 'vspaz'
 __maintainer__ = 'vspaz'
 __version__ = '0.1.3'
